a021b98d4ca77b09154f10b3b0217ec4a59ef860
Arkazix <hugohamon27@gmail.com>
2022-03-07 16:26:47 +0100

    clean up

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-from interpreter import Interpreter
-from parser_ import Parser
-from lexer import Lexer
-
-
-def read_line(text: str, line_number: int) -> None:
-    try:
-        lexer = Lexer(text)
-        tokens = lexer.generate_tokens()
-        parser = Parser(tokens)
-        tree = parser.parse()
-        if not tree: return
-        interpreter = Interpreter()
-        value = interpreter.visit(tree)
-        print(f"{line_number + 1}: {value}")
-    except Exception as e:
-        print(e)
+def read_line(line, idx):
+    pass
